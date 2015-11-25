19c32136dcbd13aa5cea13333254f18f40cb7954
MrBean1709 <levu1709@gmail.com>
2015-11-25 21:28:21 +0700

    Add entity and

--- ServicePOS/IProductService.cs
+++ ServicePOS/IProductService.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ServicePOS.Model;
 
 namespace ServicePOS
 {
-    class IProductService
+    public class IProductService : IDisposable
     {
+        IEnumerable<ProductionModel> GetProductsList();
     }
 }
