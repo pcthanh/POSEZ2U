da6078e62777fddff97c11afe01a27431cae8cf8
MrBean1709 <levu1709@gmail.com>
2015-12-03 21:38:21 +0700

    store dataMapModifireToProduct

---
+++ Document/pos_th_SaveDataMapModifireToProduct.sql
@@ -0,0 +1,76 @@
+USE [POSEZ2U]
+GO
+
+/****** Object:  StoredProcedure [dbo].[pos_th_SaveDataMapModifireToProduct]    Script Date: 12/03/2015 9:36:00 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[pos_th_SaveDataMapModifireToProduct]
+	-- Add the parameters for the stored procedure here
+   @productid INT = 0 ,
+    @userid INT = 0 ,
+    @tablemodifire TableTemp READONLY
+AS
+    BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+        SET NOCOUNT ON;
+
+
+		--SELECT * FROM @tablecategory
+
+		    UPDATE  MAP_MODIFIRE_TO_PRODUCT
+        SET     Status = 1 ,
+                UpdateBy = @userid ,
+                UpdateDate = GETDATE()
+        WHERE   ProductID = @productid AND Status=0
+                AND ModifireID IN ( SELECT  Value
+                                        FROM   @tablemodifire )
+
+        INSERT  INTO dbo.MAP_MODIFIRE_TO_PRODUCT
+                ( ModifireID ,
+                  ProductID ,
+                  Status ,
+                  Note ,
+                  CreateBy ,
+                  CreateDate ,
+                  UpdateBy ,
+                  UpdateDate
+	            )
+                SELECT  Value ,
+                        @productid ,
+                        1 ,
+                        '' ,
+                        @userid ,
+                        GETDATE() ,
+                        @userid ,
+                        GETDATE()
+                FROM    @tablemodifire
+                WHERE   Value NOT IN ( SELECT   ModifireID
+                                       FROM     dbo.MAP_MODIFIRE_TO_PRODUCT
+                                       WHERE    ProductID = @productid
+                                         )
+
+
+        UPDATE  MAP_MODIFIRE_TO_PRODUCT
+        SET     Status = 0 ,
+                UpdateBy = @userid ,
+                UpdateDate = GETDATE()
+        WHERE   ProductID = @productid
+                AND ModifireID NOT IN ( SELECT  Value
+                                        FROM    @tablemodifire )
+
+    END
+
+
+GO
+
+
